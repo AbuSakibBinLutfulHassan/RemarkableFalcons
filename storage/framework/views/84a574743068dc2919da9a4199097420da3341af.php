5a06c3f352df8484f23b0be83ce7e8345f4ed043
SHAKIB SAHARIAR SHOWMIK <94044208+SAHARIARSHOWMIK@users.noreply.github.com>
2022-11-24 14:02:24 +0800

    Add files via upload

---
+++ storage/framework/views/84a574743068dc2919da9a4199097420da3341af.php
@@ -0,0 +1,40 @@
+<main id="main">
+		<div class="container">
+
+			<!--MAIN SLIDE-->
+			<div class="wrap-main-slide">
+				<div class="slide-carousel owl-carousel style-nav-1" data-items="1" data-loop="1" data-nav="true" data-dots="false">
+					<div class="item-slide">
+						<img src="<?php echo e(asset('assets/images/main-slider-1-1.jpg')); ?>" alt="" class="img-slide">
+					
+					</div>
+					
+			</div>
+
+			<!--BANNER-->
+			<div class="wrap-banner style-twin-default">
+				<div class="banner-item">
+					<a href="#" class="link-banner banner-effect-1">
+						<figure><assets/images/home-1-banner-1.jpg" alt="" width="580" height="190"></figure>
+					</a>
+				</div>
+				<div class="banner-item">
+					<a href="#" class="link-banner banner-effect-1">
+						<figure><assets/images/home-1-banner-2.jpg" alt="" width="580" height="190"></figure>
+					</a>
+				</div>
+			</div>
+
+			
+
+			
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>			
+
+		</div>
+
+	</main><?php /**PATH C:\xampp\htdocs\OURADPROJECT\resources\views/livewire/home-component.blade.php ENDPATH**/ ?>

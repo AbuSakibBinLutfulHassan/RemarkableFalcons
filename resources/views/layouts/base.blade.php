93ae8df72c29f46562333af3051c0cbc3eeb9884
powerpuff2000 <93398334+powerpuff2000@users.noreply.github.com>
2022-12-22 15:31:17 +0800

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -17,7 +17,7 @@
 	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/color-01.css')}}">
     @livewireStyles
 </head>
-<body class="home-page home-01 ">
+<body class="home-page home-00 ">
 
 	<!-- mobile menu -->
     <div class="mercado-clone-wrap">

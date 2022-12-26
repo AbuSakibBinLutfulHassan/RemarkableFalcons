5f307756106896bd38fb736c64e9332ed3d119ef
powerpuff2000 <93398334+powerpuff2000@users.noreply.github.com>
2022-12-27 02:38:11 +0800

    Update cart-component.blade.php

--- resources/views/livewire/user/cart-component.blade.php
+++ resources/views/livewire/user/cart-component.blade.php
@@ -65,13 +65,13 @@
         <div class="summary">
             <div class="order-summary">
                 <h4 class="title-box">Order Summary</h4>
-                <p class="summary-info"><span class="title">Subtotal</span><b class="index">$512.00</b></p>
+                <p class="summary-info"><span class="title">Subtotal</span><b class="index">$610.00</b></p>
                 <p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
-                <p class="summary-info total-info "><span class="title">Total</span><b class="index">$512.00</b></p>
+                <p class="summary-info total-info "><span class="title">Total</span><b class="index">$610.00</b></p>
             </div>
             <div class="checkout-info">
                 <label class="checkbox-field">
-                    <input class="frm-input " name="have-code" id="have-code" value="" type="checkbox"><span>I have promo code</span>
+                    <input class="frm-input " name="have-code" id="have-code" value="" type="checkbox"><span>I have a promo code</span>
                 </label>
                 <a class="btn btn-checkout" href="checkout.html">Check out</a>
                 <a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>

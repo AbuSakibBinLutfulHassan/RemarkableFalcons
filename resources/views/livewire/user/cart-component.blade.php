bff2bccb7cf5a1e8931f41aa0cd7cde261d04824
powerpuff2000 <93398334+powerpuff2000@users.noreply.github.com>
2023-01-04 15:09:00 +0800

    Update cart-component.blade.php

--- resources/views/livewire/user/cart-component.blade.php
+++ resources/views/livewire/user/cart-component.blade.php
@@ -53,7 +53,7 @@
                     </div>
                     <div class="price-field sub-total"><p class="price">$256.00</p></div>
                     <div class="delete">
-                       
+                        <a href="#" class="btn btn-delete" title="">
                             <span>Delete from your cart</span>
                             <i class="fa fa-times-circle" aria-hidden="true"></i>
                         </a>

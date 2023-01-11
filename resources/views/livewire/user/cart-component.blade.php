e86db7ba3a91d4d899381a0060e7435e7719b87c
powerpuff2000 <93398334+powerpuff2000@users.noreply.github.com>
2023-01-12 00:30:29 +0800

    Update cart-component.blade.php

--- resources/views/livewire/user/cart-component.blade.php
+++ resources/views/livewire/user/cart-component.blade.php
@@ -64,7 +64,7 @@
 
         <div class="summary">
             <div class="order-summary">
-                <h4 class="title-box">Order Summary</h4>
+                <h2 class="title-box">Order Summary</h2>
                 <p class="summary-info"><span class="title">Subtotal</span><b class="index">$512.00</b></p>
                 <p class="summary-info"><span class="title">Shipping</span><b class="index">Free Shipping</b></p>
                 <p class="summary-info total-info "><span class="title">Total</span><b class="index">$512.00</b></p>

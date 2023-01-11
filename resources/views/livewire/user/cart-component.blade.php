8e163c8388e6278643f0d2b00768552304a186b1
powerpuff2000 <93398334+powerpuff2000@users.noreply.github.com>
2023-01-12 02:05:44 +0800

    Update cart-component.blade.php

--- resources/views/livewire/user/cart-component.blade.php
+++ resources/views/livewire/user/cart-component.blade.php
@@ -2,7 +2,7 @@
 
 <div class="container">
 
-    <div class="Wrap-breadcrumb">
+    <div class="wrap-breadcrumb">
         <ul>
             <li class="item-link"><a href="#" class="link">home</a></li>
             <li class="item-link"><span>Login</span></li>

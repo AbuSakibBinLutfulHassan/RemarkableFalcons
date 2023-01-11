eeb444a29b2f9f4551ab6bce0fae0fd1551fc83c
powerpuff2000 <93398334+powerpuff2000@users.noreply.github.com>
2023-01-12 01:58:10 +0800

    Update cart-component.blade.php

--- resources/views/livewire/user/cart-component.blade.php
+++ resources/views/livewire/user/cart-component.blade.php
@@ -13,7 +13,7 @@
         <div class="wrap-iten-in-cart">
             <h3 class="box-title">Product's Name</h3>
             <ul class="products-cart">
-                <li class="pc-cart-item">
+                <li class="pr-cart-item">
                     <div class="product-image">
                         <figure><img src="{{asset ('assets/images/products/digital_18.jpg')}}" alt=""></figure>
                     </div>

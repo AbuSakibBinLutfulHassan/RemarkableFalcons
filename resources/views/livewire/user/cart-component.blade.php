f002987f3243d9e186fa371eaec707893166c7b7
powerpuff2000 <93398334+powerpuff2000@users.noreply.github.com>
2023-01-12 00:35:08 +0800

    Update cart-component.blade.php

--- resources/views/livewire/user/cart-component.blade.php
+++ resources/views/livewire/user/cart-component.blade.php
@@ -105,7 +105,7 @@
                         </div>
                     </div>
 
-                    <div class="Product product-style-2 equal-elem ">
+                    <div class="product product-style-2 equal-elem ">
                         <div class="product-thumnail">
                             <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
                                 <figure><img src="{{asset ('assets/images/products/digital_17.jpg')}}" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>

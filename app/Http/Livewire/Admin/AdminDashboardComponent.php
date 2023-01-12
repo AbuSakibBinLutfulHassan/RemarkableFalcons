aac9b2afd309d9e76d43bcce629a23d5d81bd2f8
powerpuff2000 <93398334+powerpuff2000@users.noreply.github.com>
2023-01-12 15:36:19 +0800

    Update AdminDashboardComponent.php

--- app/Http/Livewire/Admin/AdminDashboardComponent.php
+++ app/Http/Livewire/Admin/AdminDashboardComponent.php
@@ -4,7 +4,7 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 
-class AdminDashboardComponent extends component
+class AdminDashboardComponent extends Component;
 {
     public function render()
     {

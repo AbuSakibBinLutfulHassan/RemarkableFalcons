f8b6ace4572e6782614440391fe545bd9f2b83b0
powerpuff2000 <93398334+powerpuff2000@users.noreply.github.com>
2023-01-16 20:21:25 +0800

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -24,7 +24,7 @@
         <div class="mercado-panels-actions-wrap">
             <a class="mercado-close-btn mercado-close-panels" href="#">x</a>
         </div>
-        <div class="mercado-panels"></div>
+        <div class=""></div>
     </div>
 
 	
